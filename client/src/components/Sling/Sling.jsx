687dddeb9f04af0a2f12267832712627535167a3
Avi Cohen <acemail10@gmail.com>
2018-01-19 11:58:59 -0800

    [add] - add modals for winning and losing duel

--- client/src/components/Sling/Sling.jsx
+++ client/src/components/Sling/Sling.jsx
@@ -6,7 +6,8 @@ import { throttle } from 'lodash';
 
 import Stdout from './StdOut/index.jsx';
 import EditorHeader from './EditorHeader';
-import Button from '../globals/Button';
+import ClassicButton from '../globals/Button';
+import { Modal, Button } from 'react-bootstrap';
 
 import 'codemirror/mode/javascript/javascript.js';
 import 'codemirror/lib/codemirror.css';
@@ -14,20 +15,22 @@ import 'codemirror/theme/base16-dark.css';
 import './Sling.css';
 
 class Sling extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       id: null,
-      ownerText: null,
-      challengerText: null,
-      text: '',
-      challenge: '',
-      stdout: ''
+      ownerText: null, //the text in user's code editor
+      challengerText: null, //the text in opponent's code editor
+      text: '', //I think this is not used? 
+      challenge: '', //the challenge selected from the challenge table.
+      stdout: '', //the user's console-logged text that appears above Run Code when click Run Code
+      challengeWinner: 'none' // added. once someone wins the challenge, the appropriate 'winner' or 'loser' modal
+                              //will display to let the users know.
     }
   }
 
-  componentDidMount() {
   
+  componentDidMount() {
     const { socket, challenge } = this.props;
     const startChall = typeof challenge === 'string' ? JSON.parse(challenge) : {}
     socket.on('connect', () => {
@@ -35,58 +38,64 @@ class Sling extends Component {
     });
     
     socket.on('server.initialState', ({ id, text, challenge }) => {
+      console.log('this is challenge', challenge);
       this.setState({
         id,
         ownerText: text,
         challengerText: text,
         challenge
-      }, ()=>{console.log('Sling.jsx on component mounted --> \n\n this.state:', this.state, '\n\nthis.props: ', this.props, '\n\ntypeofthis.props.location.state.testCases[0].content', typeof this.props.location.state.testCases[0].content)});
+      });
     });
-
+    
     socket.on('server.changed', ({ text, email }) => {
+      console.log('text and email', text, email);
       if (localStorage.getItem('email') === email) {
         this.setState({ ownerText: text });
       } else {
         this.setState({ challengerText: text });
       }
     });
-
+    
+    //here is where client is receiving output of running code. so this is most likely where'd get if all tests pass.
+    //to update challengeWinnerstate.
+    //this gets the output for both challenge users. it just only sets state for stdout
+    //when the email on the data matches this users email addy
     socket.on('server.run', ({ stdout, email }) => {
       const ownerEmail = localStorage.getItem('email');
-
-      console.log('i am standard output:', stdout)
       email === ownerEmail ? this.setState({ stdout }) : null;
     });
-
+    
     window.addEventListener('resize', this.setEditorSize);
   }
-
+  
   submitCode = () => {
     const { socket } = this.props;
     const { ownerText } = this.state;
-    const testCases = this.props.location.state.testCases;
     const email = localStorage.getItem('email');
-
-    // emit to the server when a user wants to run code in editor.
-    
-    socket.emit('client.run', { text: ownerText, email, testCases});
+    socket.emit('client.run', { text: ownerText, email });
   }
-
+  
   handleChange = throttle((editor, metadata, value) => {
     const email = localStorage.getItem('email');
     this.props.socket.emit('client.update', { text: value, email });
   }, 250)
-
+  
   setEditorSize = throttle(() => {
     this.editor.setSize(null, `${window.innerHeight - 80}px`);
   }, 100);
-
+  
   initializeEditor = (editor) => {
     this.editor = editor;
     this.setEditorSize();
   }
+  
+  //for testing buttons and to reset challenge winner to 'none' once modal is closed just in case that will be useful
+  setChallengeWinner(arg){
+      this.setState({challengeWinner: arg});
+  }
 
   render() {
+    
     const { socket } = this.props;
     return (
       <div className="sling-container">
@@ -104,11 +113,50 @@ class Sling extends Component {
             />
         </div>
         <div className="stdout-container">
+           
+      <div>
+				<Modal
+					show={this.state.challengeWinner === 'winner' ? true : false}
+          animation={true}
+          className="modal"
+          bsSize="large"
+				>
+          <Modal.Dialog>
+            <Modal.Header>
+              <Modal.Title>Congrats!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              You won the challenge! Woohoo!!!
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => this.setChallengeWinner('none')}>Close</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+				</Modal>
+        <Modal
+					show={this.state.challengeWinner === 'loser' ? true : false}
+          animation={true}
+          className="modal"
+          bsSize="large"
+				>
+          <Modal.Dialog>
+            <Modal.Header>
+              <Modal.Title>Sorry!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              You lose. Your opponent beat you.
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => this.setChallengeWinner('none')}>Close</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+				</Modal>
+			</div>
             {this.state.challenge.title || this.props.challenge.title}
             <br/>
             {this.state.challenge.content || this.props.challenge.content}
           <Stdout text={this.state.stdout}/>
-          <Button
+          <ClassicButton
             className="run-btn"
             text="Run Code"
             backgroundColor="red"
@@ -116,7 +164,6 @@ class Sling extends Component {
             onClick={() => this.submitCode()}
           />
         </div>
-        
         <div className="code2-editor-container">
           <CodeMirror 
             editorDidMount={this.initializeEditor}
@@ -135,6 +182,3 @@ class Sling extends Component {
 }
 
 export default Sling;
-
-
-
